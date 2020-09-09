db53fd33dd3534bfab612b89858ec85f86793d9e
marinambur <marinaburykina@gmail.com>
2020-09-10 00:09:46 +0300

    js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import '../pages/index.css';
 
-const page = document.querySelector('.page')//нaходим класс page
+const page = document.querySelector('.page')
 const contents = Array.from(document.querySelectorAll(".content__container"));
 const lineLeft = Array.from(document.querySelectorAll(".line_left"));
 const lines = Array.from(document.querySelectorAll(".line"));
@@ -11,12 +11,21 @@ const pic = Array.from(document.querySelectorAll(".content__pic"));
 const fridge = document.querySelector(".lead__fridge");
 const leadTitle = document.querySelector(".lead__title");
 const logo = document.querySelector(".header__logo");
+const slider = document.querySelector(".slider__box");
+const lead = document.querySelector(".lead");
+
+
 if (document.documentElement.clientWidth < 1100) {
     page.classList.add('small-pc');
+    slider.classList.add('slider__box_medium');
+    contents.forEach((content) => {
+        content.classList.add('content__container_tablet')
+    });
 }
 
 if (document.documentElement.clientWidth < 992) {
     page.classList.add('tablet');
+    slider.classList.add('slider__box_small');
 }
 
 
@@ -25,6 +34,7 @@ if (document.documentElement.clientWidth < 768) {
     fridge.classList.add('lead__fridge_mobile');
     leadTitle.classList.add('lead__title_mobile');
     logo.classList.add('header__logo_mobile');
+    lead.classList.add('lead_mobile');
     contents.forEach((content) => {
         content.classList.add('content__container_mobile')
     });
@@ -47,7 +57,4 @@ if (document.documentElement.clientWidth < 768) {
         pic.classList.add('content__pic_mobile')
     });
 }
-console.log(document.documentElement.clientWidth);
 
-/*slider__box {
-    width: 300px;*/
